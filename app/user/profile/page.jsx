69c77ee75d091349d4cac3a99b2c9dd3b1b7c9e4
ankitroy1998 <ankit.roy98@yahoo.com>
2024-04-08 19:31:13 +0530

    Errors Fixed

--- app/user/profile/page.jsx
+++ app/user/profile/page.jsx
@@ -5,13 +5,15 @@ import { useEffect,useState } from 'react';
 import { useRouter } from 'next/navigation'
 import { useCookies } from "react-cookie";
 import HashLoader from "react-spinners/HashLoader";
+import * as Yup from 'yup';
 
 export default function Home() {
 
   const router = useRouter();
   const [data, setData] = useState("");
   const cookies = useCookies("");
-
+  const [error, setError] = useState('');
+  
   useEffect(()=>{
     if(!cookies[0].username){
       router.push('/user/login')
@@ -28,6 +30,23 @@ if (!data) {
   // Render loading state or return null
   return <div className='flex justify-center items-center h-screen'><HashLoader color={"#8c97f9"} loading={true} size={50} speedMultiplier={2}/></div>;
 }
+
+  const validate = Yup.object().shape({
+    Gender: Yup.string()
+      .required('Gender is required'),
+    Age: Yup.string()
+      .required('Age is required'),
+    Height: Yup.string()
+      .required('Height is required'),
+    Weight: Yup.string()
+      .required('Weight is required'),
+    Duration: Yup.string()
+      .required('Duration is required'),
+    HeartRate: Yup.string()
+      .required('Heart Rate is required'),
+    Temperature: Yup.string()
+      .required('Body Temperature is required')
+  });
 
   const handleData = async (values) => {
     try{
@@ -50,36 +69,43 @@ if (!data) {
         <Formik
         initialValues={
               {
-                Gender: 'Male',
-                Age: '29',
-                Height: '172',
-                Weight: '45',
-                Duration: '30',
-                HeartRate : '85',
-                Temperature: '37'
+                Gender: '',
+                Age: '',
+                Height: '',
+                Weight: '',
+                Duration: '',
+                HeartRate : '',
+                Temperature: '',
               }
             }
-            // validationSchema={validationSchema}
+            validationSchema={validate}
             onSubmit={values => {handleData(values)}}
           >
             {({ errors, touched })=>(
           <Form>
             <label htmlfor="Gender" className="lg:text-base sm:block text-sm mb-2">Gender</label>
-            <Field type="text" name="Gender" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="text" name="Gender" placeholder="Male/Female" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Gender}</h4>
             <label htmlfor="Age" className="lg:text-base sm:block text-sm mb-2">Age</label>
-            <Field type="number" name="Age" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="Age" placeholder="29" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Age}</h4>
             <label htmlfor="Height" className="lg:text-base sm:block text-sm mb-2">Height</label>
-            <Field type="number" name="Height" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="Height" placeholder="172" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Height}</h4>
             <label htmlfor="Weight" className="lg:text-base sm:block text-sm mb-2">Weight</label>
-            <Field type="number" name="Weight" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="Weight" placeholder="45" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Weight}</h4>
             <label htmlfor="Duration" className="lg:text-base sm:block text-sm mb-2">Duration</label>
-            <Field type="number" name="Duration" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="Duration" placeholder="30" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Duration}</h4>
             <label htmlfor="HeartRate" className="lg:text-base sm:block text-sm mb-2">Heart Rate</label>
-            <Field type="number" name="HeartRate" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="HeartRate" placeholder="85" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.HeartRate}</h4>
             <label htmlfor="Temperature" className="lg:text-base sm:block text-sm mb-2">Temperature</label>
-            <Field type="number" name="Temperature" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <Field type="number" name="Temperature" placeholder="37" className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
+            <h4 className='text-xs text-red-600 mb-1'>{errors.Temperature}</h4>
             <button type="submit" className="mt-4 w-full bg-indigo-500 text-white p-3 rounded-md">Submit</button>
-            <label htmlfor="result" className="lg:text-base sm:block text-sm mb-2">Result</label>
+            <label htmlfor="result" className="lg:text-base sm:block text-sm mb-2 mt-1">Result</label>
             <Field type="number" name="result" value={data} className="lg:text-base sm:border w-full text-xs px-2 py-1 focus:outline none focus:ring-0 focus:border-indigo-400 rounded-md" />
           </Form>
             )}
